8ef9a7082079f1ed04d00d9f83dbc791ac2e0d42
ReOpAu <admin@reop.com.au>
2025-07-14 15:20:18 +1000

    feat(homepage): add new components from about-pages-styles

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,15 @@
 import { getAuth } from "@clerk/react-router/ssr.server";
 import type { Route } from "./+types/home";
 import { Conversation } from "~/components/conversation";
-import ContentSection from "~/components/homepage/content";
+import { Hero } from "~/components/home/Hero";
+import { Features } from "~/components/home/Features";
+import { InvestorTypes } from "~/components/home/InvestorTypes";
+import { BuyerGuide } from "~/components/home/BuyerGuide";
+import { SellerGuide } from "~/components/home/SellerGuide";
+import { HowItWorks } from "~/components/home/HowItWorks";
+import { CallToAction } from "~/components/home/CallToAction";
 import Footer from "~/components/homepage/footer";
-import Integrations from "~/components/homepage/integrations";
 import { Navbar } from "~/components/homepage/navbar";
-import Pricing from "~/components/homepage/pricing";
-import Team from "~/components/homepage/team";
 
 export async function loader(args: Route.LoaderArgs) {
 	const { userId } = await getAuth(args);
@@ -19,14 +22,17 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
 	return (
 		<>
 			<Navbar loaderData={loaderData} />
-			<main className="container mx-auto px-4 py-8">
+			<main>
+				<Hero />
+				<Features />
+				<InvestorTypes />
 				<div className="my-12">
 					<Conversation />
 				</div>
-				<Integrations />
-				<ContentSection />
-				<Team />
-				<Pricing />
+				<BuyerGuide />
+				<SellerGuide />
+				<HowItWorks />
+				<CallToAction />
 			</main>
 			<Footer />
 		</>
