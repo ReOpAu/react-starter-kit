487eeb9eddd0d6226bdc381a1e50fafc19bf681a
ReOpAu <admin@reop.com.au>
2025-07-14 15:18:53 +1000

    fix(homepage): add Conversation component and fix layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,12 @@
 import { getAuth } from "@clerk/react-router/ssr.server";
 import type { Route } from "./+types/home";
-import { Navbar } from "~/components/homepage/navbar";
-import  Footer from "~/components/homepage/footer";
+import { Conversation } from "~/components/conversation";
 import ContentSection from "~/components/homepage/content";
+import Footer from "~/components/homepage/footer";
 import Integrations from "~/components/homepage/integrations";
+import { Navbar } from "~/components/homepage/navbar";
 import Pricing from "~/components/homepage/pricing";
 import Team from "~/components/homepage/team";
-
 
 export async function loader(args: Route.LoaderArgs) {
 	const { userId } = await getAuth(args);
@@ -16,16 +16,19 @@ export async function loader(args: Route.LoaderArgs) {
 }
 
 export default function HomePage({ loaderData }: Route.ComponentProps) {
-  return (
-    <>
-      <Navbar loaderData={loaderData} />
-      <main>
-        <Integrations />
-        <ContentSection />
-        <Team />
-        <Pricing />
-      </main>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Navbar loaderData={loaderData} />
+			<main className="container mx-auto px-4 py-8">
+				<div className="my-12">
+					<Conversation />
+				</div>
+				<Integrations />
+				<ContentSection />
+				<Team />
+				<Pricing />
+			</main>
+			<Footer />
+		</>
+	);
 }
